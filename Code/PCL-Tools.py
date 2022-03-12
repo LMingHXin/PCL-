b4094bb2a5f7abb1a6a53efd7d2c4e9647ba318f
LMingHXin <92614729+LMingHXin@users.noreply.github.com>
2022-03-12 11:52:26 +0800

    重构后代码

--- Code/PCL-Tools.py
+++ Code/PCL-Tools.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 from hashlib import new
-from tkinter.messagebox import askokcancel, showinfo, showwarning
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import tkinter as tk, json, os
+import tkinter as tk, os
 from tkinter import *
-
+class launcher(tk.Tk):
+    def __init__(self, parent = None):
+        super().__init__()
+        self.title("PCL工具箱")
+        self.setup_UI()
 #组件对应逻辑
-def goto_cave():
-    os.popen('python cave.py')
-
-#GUI设计
-root = tk.Tk()
-root.title("PCL工具箱")
-but1 = Button(root, text = "回声洞快速提交", command = goto_cave)
-but1.grid(row = 0, column = 0, columnspan = 6, ipadx = 60, ipady = 10, padx = 30, pady = 15)
-tk.mainloop()
+    def goto_cave(self):
+        os.popen('python cave.py')
+#GUI
+    def setup_UI(self):
+        but1 = Button(self, text = "回声洞快速提交", command = self.goto_cave)
+        but1.grid(row = 0, column = 0, columnspan = 6, ipadx = 60, ipady = 10, padx = 30, pady = 15)
+if __name__ == '__main__':
+    launcher().mainloop()
